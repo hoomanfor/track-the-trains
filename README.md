138b514360d1d1b7f62b9a0c1c3b130ad999e68f
Hoo Man <hoomanfor@gmail.com>
2019-10-05 11:28:54 -0400

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# track-the-trains
+# Track the Trains!
+
+Check it out: [Track the Trains!](https://hoomanfor.github.io/track-the-trains/)
+
+Work in progres... :-D
---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB-ouDmd2b0kGRbaZW3vprnml6pZMD9_s0",
+    authDomain: "track-the-trains.firebaseapp.com",
+    databaseURL: "https://track-the-trains.firebaseio.com",
+    projectId: "track-the-trains",
+    storageBucket: "",
+    messagingSenderId: "786993245325",
+    appId: "1:786993245325:web:2a935c8a243842e6d946c4"
+};
+
+firebase.initializeApp(firebaseConfig);
